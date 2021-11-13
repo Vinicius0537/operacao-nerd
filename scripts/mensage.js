4b1cc70e2c377dbd160160ff366bc4fb09a39650
Vinicius0537 <Pereiravinicius0537@gmail.com>
2021-11-13 13:35:16 -0300

    Adicionando a pasta scripts

---
+++ scripts/mensage.js
@@ -0,0 +1,14 @@
+function mensage() {
+    const showmensage = [     
+        `
+        Nomes:
+
+        José Vinicius Pereira de Rezende,
+        Matheus Lopes,
+        Lucas Barros
+        
+        Turma: 3º TI
+        `
+    ]
+    window.alert(showmensage);
+}
